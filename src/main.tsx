ba770a0ea4cb320f7769cb110e3cedaef2e89280
Syedfayaz Mujawar <syedfayaz_mujawar@epam.com>
2022-12-17 21:24:03 +0530

    updated package name

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/react-notes-app'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
